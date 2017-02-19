ee12eb0024ffff1fc624a8c91afdc7dae5a2d7a3
Mohammad121363 <mohammad76327@gmail.com>
2017-02-19 09:38:36 -0800

    Update on bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,8 @@
 <?php
 $bot_id = "301178619:AAGXeirbohMzKfn9mg2FgatgLgGDJgtI6t4";
 echo $bot_id;
+include("Telegram.php");
+$telegram = new Telegram($bot_id);
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => "Test");
+$telegram->sendMessage($content);
