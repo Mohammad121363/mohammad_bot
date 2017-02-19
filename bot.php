6eb73574eaf37b48ec4e8a19eec90c8de1bb51b9
Mohammad121363 <mohammad76327@gmail.com>
2017-02-19 14:03:24 -0800

    Update change on bot.php

--- bot.php
+++ bot.php
@@ -6,9 +6,9 @@ $text = $update->message->text;
 
 function makecurl($method, $datas, $api){
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 	"https://api.telegram.org/bot{$api}/{$method}");
+curl_setopt($ch, CURLOPT_URL, 	"https://api.telegram.org/bot{$api}/{$method}");
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, 	CURLOPT_POSTFIELDS,fttp_build_query($datas);
+curl_setopt($ch, 	CURLOPT_POSTFIELDS,fttp_build_query($datas);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$server_output = curl_exec($ch);
 	curl_close($ch);
@@ -22,6 +22,7 @@ makecurl(
 			'reply_to_message_id'=>$message_id,
 			'parse_mode'=>'HTML' //we want to reply HTML Message :P
 		]
+		,
 		'301178619:AAGXeirbohMzKfn9mg2FgatgLgGDJgtI6t4'
 	);
 ?>
