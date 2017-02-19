f4e895a538bf129f78fc6a863d57d28a5e3ed2c7
Mohammad121363 <mohammad76327@gmail.com>
2017-02-19 14:40:04 -0800

    Update 11 change on bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ function makecurl($method, $datas, $api){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot{$api}/{$method}");
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS,fttp_build_query($datas));
+	curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($datas));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$server_output = curl_exec($ch);
 	curl_close($ch);
